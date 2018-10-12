701e3e7900eeeeaa3b1ffc89f7ddd3efb65dbdc6
starfish01 <patrick.labes@gmail.com>
2018-10-13 09:11:14 +1000

    morning lessons

---
+++ Section 8/Section Code (Start)/src/components/UserDetail.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ switchName(name) }}</p>
+        <button @click="resetName">Reset Name</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:{
+            name: {
+                type: String,
+                required: true
+            }
+        },
+        methods:{
+            switchName() {
+                return this.name.split("").reverse().join("");
+            },
+            resetName() {
+                this.name = 'Max';
+                this.$emit('nameWasReset', this.name);
+            }
+        }
+    }
+
+
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
