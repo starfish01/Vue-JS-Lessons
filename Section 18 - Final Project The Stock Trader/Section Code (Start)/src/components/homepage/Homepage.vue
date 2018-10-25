4ed59cee640b59fce1788e054318240b3938825b
starfish01 <patrick.labes@gmail.com>
2018-10-26 06:33:36 +1000

    set up vuex

--- Section 18 - Final Project The Stock Trader/Section Code (Start)/src/components/homepage/Homepage.vue
+++ Section 18 - Final Project The Stock Trader/Section Code (Start)/src/components/homepage/Homepage.vue
@@ -1,14 +1,25 @@
 <template>
     <div class="panel panel-default">
         <div class="panel-body">
-        homepage
+        <h3>Trade or View your Portfolio</h3>
+        <p>You may Save & Load your Data</p>
+        <p>Click on 'End Day' to begin a new Day!</p>
+        <hr>
+        <h4>Your Funds: ${{ walletValue }}</h4>
         </div>
     </div>
 </template>
 
 <script>
-export default {
 
+import { mapGetters } from 'vuex'
+
+export default {
+    computed:{
+        ...mapGetters([
+            'walletValue'
+        ])
+    }
 }
 </script>
 
--- Section 18 - Final Project The Stock Trader/Section Code (Start)/src/store/store.js
+++ Section 18 - Final Project The Stock Trader/Section Code (Start)/src/store/store.js
@@ -2,3 +2,14 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+	state:{
+		wallet: 10000
+    },
+    getters:{
+        walletValue: state =>{
+            return state.wallet;
+        }
+    }
+})
