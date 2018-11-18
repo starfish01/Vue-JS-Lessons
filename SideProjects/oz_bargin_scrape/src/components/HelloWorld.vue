e75906b5ce9be38d54b492a8da3c489567bcb8cc
Patrick Labes <patrick.labes@gmail.com>
2018-11-18 16:49:16 +1000

    need to pass the data back

--- SideProjects/oz_bargin_scrape/src/components/HelloWorld.vue
+++ SideProjects/oz_bargin_scrape/src/components/HelloWorld.vue
@@ -1,14 +1,27 @@
 <template>
   <v-container>
-          
+          <p>{{hello}}</p>
+          <button @click="getData">get data</button>
+          <p>{{data}}</p>
   </v-container>
 </template>
 
 <script>
+import {ozSiteData} from '../../src/scrape.js'
+
   export default {
     data(){
       return{
-        
+        hello:"hello",
+        data:0
+      }
+    },
+    computed:{
+      
+    },
+    methods:{
+      getData(){
+        this.data = ozSiteData;
       }
     }
   }
--- SideProjects/oz_bargin_scrape/src/scrape.js
+++ SideProjects/oz_bargin_scrape/src/scrape.js
@@ -1,24 +1,7 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-// request('https://news.ycombinator.com', function (error, response, html) {
-//   if (!error && response.statusCode == 200) {
-//     console.log(html);
-//   }
-// });
 
-// request('https://news.ycombinator.com', function (error, response, html) {
-//   if (!error && response.statusCode == 200) {
-//     var $ = cheerio.load(html);
-//     $('span.comhead').each(function(i, element){
-//       var a = $(this).prev();
-//       console.log(a.text());
-//     });
-//   }
-// });
-
-
-//for ozbargin
 
 request('https://www.ozbargain.com.au/', function(error, response,html){
     if(!error && response.statusCode == 200){
@@ -32,30 +15,40 @@ request('https://www.ozbargain.com.au/', function(error, response,html){
             var description = $(this).siblings('div.content').children('p').text()
             //grabs link
             var link = $(this).children('a').attr('href')
-            //grab expiry commentCount
-            var comments = $(this).siblings('div.links').children().children().clone().children().remove().end().text().trim()
-            //grab expirydate
-            var expirydate = $(this).siblings('div.links').children().children().children().clone().children().remove().end().text().trim()
-
-            //grab tags
-            var tags =  $(this).siblings('div.links').children().html()
-            // .clone().children().remove().end().text().trim()
-
-            //probably could do something more interesting with this because
-            //it also grabs in 7 days and out of stock and expired
-            var infoTAG = $(this).children('span').text()
-            // console.log(expired)
-            //check if its a deal and not anything else
-            if(deal && !infoTAG ){
-                console.log('---------------')
-                console.log(title)
-                console.log(description)
-                console.log(link)
-                console.log(comments)
-                console.log(expirydate)
-                console.log(tags)
+            var taggsArray = []
+            $(this).siblings('div.links').children().find('li').each(function() {
+               taggsArray.push($(this).text().trim()) 
+            });
+            var comments = taggsArray[0]
+            var tag = taggsArray[1]
+            var expirydate = taggsArray[2]
+            if(expirydate === 'Freebie'){
+                expirydate = taggsArray[3]
             }
+            if(expirydate == undefined){
+                expirydate = 0
+            }
+            
+            var infoTAG = $(this).children('span').text()
+            if(deal){
+                var metadata ={
+                    title,
+                    description,
+                    infoTAG,
+                    link,
+                    comments,
+                    tag,
+                    expirydate
+
+                }
+            }
+            return metadata
+
         })
         
     }
 })
+
+
+
+export var ozSiteData = 5
