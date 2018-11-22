dd696b0d2dc3940bf54185dbceaa132da603fca7
Patrick Labes <patrick.labes@gmail.com>
2018-11-22 14:19:05 +1000

    started main building area

--- SideProjects/Builder/src/App.vue
+++ SideProjects/Builder/src/App.vue
@@ -1,38 +1,61 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+  <v-app id="inspire" dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      clipped
+      fixed
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Project Builder</v-toolbar-title>
     </v-toolbar>
-
     <v-content>
-      <HelloWorld/>
+      
+          
+            <component :is="currentComponent"></component>
+          
     </v-content>
+    <v-footer app fixed>
+      <span>&copy; {{(new Date()).getFullYear()}}</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
+import BuilderMain from './components/Builder/BuilderMain.vue'
+ 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
+  data(){
+    return{
+      currentComponent: 'appBuilderMain',
+      props: {
+        source: String
+      }              
     }
+  },
+  components:{
+    appBuilderMain:BuilderMain
   }
 }
 </script>
---
+++ SideProjects/Builder/src/components/Builder/BuilderMain.vue
@@ -0,0 +1,23 @@
+<template>
+   <v-container grid-list-md text-xs-left>
+    <v-layout row wrap>
+      <v-flex  xs12>
+        <v-card dark color="primary">
+          <v-card-text class="px-6"></v-card-text>
+          <v-card-text class="px-6"><v-btn  target="_blank">Open Link</v-btn></v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+
+</style>
