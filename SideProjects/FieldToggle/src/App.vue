ce8c64ea7544166ce83d0eb64486b38e32a32cbd
Patrick Labes <patrick.labes@gmail.com>
2019-01-11 14:18:47 +1000

    a little tool for work

---
+++ SideProjects/FieldToggle/src/App.vue
@@ -0,0 +1,98 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs">
+          <label>Active Field</label><br>
+          <input v-model="activeField" placeholder="">
+        </div>
+        <div class="col-xs">
+          <label>First Field</label><br>
+          <input v-model="firstField" placeholder="">
+        </div>
+        <div class="col-xs">
+          <label>Last Field</label><br>
+          <input v-model="lastField" placeholder="">
+        </div>
+        <div class="col-xs">
+          <label>conditions</label><br>
+            <div v-for="(condition, index) in conditions" :key="index"> 
+               <input v-model="conditions[index]" placeholder=""> <button type="button" class="btn btn-danger btn-sm deleteButton" @click="removeCondition(index)">Delete</button>
+            </div>
+            <img class="addButton" src="../src/assets/add.svg" @click="addCondition">
+        </div>
+        
+      </div>
+      <br>
+      <div class="row">
+        <div class="col-xs">
+          <p>forms.toggle_fields({{ activeField }}, [{{conditionFields}}], [{{conditions}}]);</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'component-name',
+  components: {},
+  data() {
+      return {
+        activeField:'',
+        firstField:'',
+        lastField:'',
+        conditions:[""],
+        fields:[]
+  }
+},
+methods: {
+  addCondition() {
+    console.log('something');
+    this.conditions.push("");
+  },
+  removeCondition(index){
+    this.conditions.splice(index, 1)
+  }
+},
+computed: {
+  conditionFields () {
+    if(this.firstField.length > 0 & this.lastField.length > 0){
+
+      return "con";
+    }
+
+    return "";
+  }
+}
+    
+}
+</script>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.addButton{
+      width: 10%;
+}
+.addButton:hover {
+  background-color: red;
+}
+.addButtonRow {
+    text-align: left;
+    padding-left: 9px;
+}
+.deleteButton{
+  margin-left: 5px;
+}
+
+
+</style>
