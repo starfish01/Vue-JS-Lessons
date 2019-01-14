4e6a17161dc73b0021f3d43f8e79add83ff1c414
starfish01 <patrick.labes@gmail.com>
2019-01-15 06:39:57 +1000

    pretty happy with my button color selector

---
+++ SideProjects/EnrolBuilder/src/components/material/ColorPicker.vue
@@ -0,0 +1,32 @@
+<template>
+    <chrome-picker v-model="color" />
+</template>
+
+<script>
+import { Chrome } from "vue-color";
+export default {
+    data() {
+        return {
+            colorValue:{hex: "#194d33"},
+        }
+    },
+    components:{
+        "chrome-picker": Chrome,
+    },
+    computed:{
+        color:{
+            get(){
+                return this.colorValue
+            },
+            set(newVal){
+                this.value = newVal
+                this.$emit('colorChange', this.value);
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
