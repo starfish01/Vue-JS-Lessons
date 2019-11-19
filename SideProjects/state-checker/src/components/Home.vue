421c75f7112e3c5de9d542e5ffa46f790db1ee9e
Patrick Labes <patrick.labes@gmail.com>
2019-11-19 14:37:23 +1000

    working on a new side project

---
+++ SideProjects/state-checker/src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <v-layout text-center wrap>
+      <v-flex xs12>
+        <v-select
+          v-model="Type"
+          :items="items"
+          label="Type"
+          item-text="title"
+          item-value="id"
+        ></v-select>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  props: {
+    msg: String
+  },
+   data: () => ({
+     items: [
+        {'title' : 'Something', 'value':'1'}
+      ]
+   })
+
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
