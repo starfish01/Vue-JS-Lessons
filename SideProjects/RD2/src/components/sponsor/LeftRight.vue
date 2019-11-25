de8dff42c7cc71b8d20e8f45efa2e7664fafead5
patrickLabes <patrick.labes@digistorm.com.au>
2019-11-25 14:36:57 +1000

    Adding support for adds

--- SideProjects/RD2/src/components/sponsor/LeftRight.vue
+++ SideProjects/RD2/src/components/sponsor/LeftRight.vue
@@ -1,7 +1,13 @@
 <template>
-  <v-row align="center" justify="center">
-    <img src="../../assets/sponsor/addlr.jpg" style="width:200px;height:auto;" />
-  </v-row>
+<v-row align="center" justify="center" style="width:200px; height:200px">
+    <!-- <img src="../../assets/sponsor/addlr.jpg" style="width:200px;height:auto;" /> -->
+    <Adsense data-full-width-responsive="true" data-ad-format="auto" data-ad-client="ca-pub-4578555681953964" data-ad-slot="4578555681953964">
+    </Adsense>
+</v-row>
 </template>
+
 <style scoped>
+Adsense {
+    display: block
+}
 </style>
--- SideProjects/RD2/src/main.js
+++ SideProjects/RD2/src/main.js
@@ -4,8 +4,23 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import vuetify from './plugins/vuetify'
+import Ads from 'vue-google-adsense'
 
 import { Icon } from "leaflet"
+
+
+Vue.use(require('vue-script2'))
+
+Vue.use(Ads.Adsense)
+Vue.use(Ads.InArticleAdsense)
+Vue.use(Ads.InFeedAdsense)
+Vue.use(Ads.AutoAdsense, { adClient: 'ca-pub-4578555681953964' })
+
+/* 
+https://adsensexplosion.wordpress.com/2015/10/20/google-adsense-data-ad-slot-where-do-i-get-it/
+Looks like i still need some add info
+*/
+
 
 Vue.config.productionTip = false
 delete Icon.Default.prototype._getIconUrl
